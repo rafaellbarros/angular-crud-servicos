fd992059ffd4177ae6b2d9ac1c2f5bcb83f2eada
Rafael Barros <rafaelbarros.df@gmail.com>
2018-09-11 22:20:57 -0300

    Criando navegação com Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: PostListComponent },
+  { path: 'posts',  component: PostListComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
