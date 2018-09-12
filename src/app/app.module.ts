3f7827a768baa443f95532ba03b2402a39d8bd5e
Rafael Barros <rafaelbarros.df@gmail.com>
2018-09-11 23:03:56 -0300

    Criando componente para botão bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { PostSaveComponent } from './post-save/post-save.component';
+import { ButtonComponent } from './bootstrap/button/button.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', component: PostListComponent },
-  { path: 'posts',  component: PostListComponent }
+  { path: 'posts',  component: PostListComponent },
+  { path: 'posts/create',  component: PostSaveComponent }
+  
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    PostSaveComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-save/post-save.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-save',
+  templateUrl: './post-save.component.html',
+  styleUrls: ['./post-save.component.css']
+})
+export class PostSaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
